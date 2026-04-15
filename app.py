1baa2b5588aa05f7e3f8b4fadbea2a8d987da1a3
omwagh356-dot <omwagh356@gmail.com>
2026-04-16 02:23:45 +0530

    Remove pip install line for streamlit
    Removed the pip install command for streamlit.

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/github/googlecolab/colabtools/blob/main/notebooks/Getting_started_with_google_colab_ai.ipynb
 """
 
-#!pip install streamlit
 import streamlit as st
 import pandas as pd
 import numpy as np
